0cf9fc9e402a0836eb90ab36318d83c1d213bea7
Koushik Shetty <kou4307@gmail.com>
2023-08-20 22:40:28 +0530

    refactor using x, y

--- 2015/q3/q3p1.go
+++ 2015/q3/q3p1.go
@@ -1,21 +1,17 @@
 package main
 
+import "fmt"
+
 func q3p1(dirs string) int {
-	curr := []int{0, 0}
-	visited := make(map[int]map[int]bool)
-	visited[curr[0]] = make(map[int]bool)
-	visited[curr[0]][curr[1]] = true
+	x, y := 0, 0
+	visited := make(map[string]bool)
+	visited[toKey(x, y)] = true
 
 	giftedHouses := 1
 	for _, c := range dirs {
-		nextLoc(curr, string(c))
-		x, y := curr[0], curr[1]
-		if xaxis, ok := visited[x]; !ok {
-			visited[x] = make(map[int]bool)
-			visited[x][y] = true
-			giftedHouses++
-		} else if yaxis, k := xaxis[y]; !yaxis || !k {
-			visited[x][y] = true
+		x, y = nextLoc(x, y, string(c))
+		if present, ok := visited[toKey(x, y)]; !ok || !present {
+			visited[toKey(x, y)] = true
 			giftedHouses++
 		}
 	}
@@ -23,15 +19,18 @@ func q3p1(dirs string) int {
 	return giftedHouses
 }
 
-func nextLoc(start []int, dir string) {
+func toKey(x, y int) string { return fmt.Sprintf("%v_%v", x, y) }
+
+func nextLoc(x, y int, dir string) (int, int) {
 	switch dir {
 	case "^":
-		start[0] += 1
+		return x + 1, y
 	case "v":
-		start[0] -= 1
+		return x - 1, y
 	case "<":
-		start[1] -= 1
+		return x, y - 1
 	case ">":
-		start[1] += 1
+		return x, y + 1
 	}
+	return x, y
 }
