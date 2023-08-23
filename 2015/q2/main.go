fb3163e8948e12d64730401af8ae0b28df816a16
Koushik Shetty <kou4307@gmail.com>
2023-08-23 10:19:54 +0530

    refator and make files consistent

--- 2015/q2/main.go
+++ 2015/q2/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Total Paper: %#v\n", q2p1("q2p1.txt"))
-	fmt.Printf("Total Ribbon: %#v\n", q2p2("q2p1.txt"))
+	fmt.Printf("Total Paper: %#v\n", q2p1("test.txt"))
+	fmt.Printf("Total Ribbon: %#v\n", q2p2("test.txt"))
 }
