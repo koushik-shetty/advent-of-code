b44244cf6060da6c66b0414c9671cd0bfd44efb8
Koushik Shetty <kou4307@gmail.com>
2023-08-23 09:53:10 +0530

    q4p1 and p2 solutions

--- 2015/q4/main.go
+++ 2015/q4/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/koushik-shetty/advent-of-code/v2/2015/utils"
+)
+
+func main() {
+	file := "q4.txt"
+	ip := utils.NewInput(file, 10000)
+	secrets, err := ip.ReadChunk()
+	if err != nil {
+		log.Fatalf("Error: %#v", err.Error())
+	}
+	fmt.Printf("Smalles positive integer to have 5 zero md5 prefix: %#v\n", q4p1(secrets))
+	fmt.Printf("Smalles positive integer to have 6 zero md5 prefix: %#v\n", q4p2(secrets))
+
+}
--- 2015/q4/q4p2.go
+++ 2015/q4/q4p2.go
@@ -8,13 +8,13 @@ import (
 	"strings"
 )
 
-func q4p1(secret string) int {
+func q4p2(secret string) int {
 	for smallestNum := 0; smallestNum <= math.MaxInt; smallestNum++ {
 		hash := md5.Sum([]byte(fmt.Sprintf("%s%v", secret, smallestNum)))
 
 		hexCode := hex.EncodeToString(hash[:])
 
-		if strings.HasPrefix(hexCode, "00000") {
+		if strings.HasPrefix(hexCode, "000000") {
 			return smallestNum
 		}
 	}
